d4ac5bfca49cad1a7ab74ff1445aa28a0f1cb11d
helraisers <helraiser@yandex.ru>
2022-05-16 16:52:00 +0300

    Added models, routing and admin panel

--- sc_web/admin.py
+++ sc_web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Brand_Product)
+admin.site.register(Model_Product)
+admin.site.register(Product)
+admin.site.register(Type_Operation)
+admin.site.register(Company)
+

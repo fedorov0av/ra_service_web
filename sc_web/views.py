885f2de6d092f059c28a837b574eaa059fe8f706
helraisers <helraiser@yandex.ru>
2022-05-18 08:39:47 +0300

    fix views

--- sc_web/views.py
+++ sc_web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from .models import Brand_Product, Model_Product, Product, Type_Operation, Company
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'table.html')
